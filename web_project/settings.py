fbb4e5177643493cd24c798c9bfb92809e01a21e
GustaveRw <jmgustave5@gmail.com>
2022-06-11 01:53:28 +0200

    Updated

--- web_project/settings.py
+++ web_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from concurrent.futures import process
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2r+xvt5by(6^69m2sxuaus1+=z8b9$-4jr#y49!jvibxat$=j7'
+SECRET_KEY = process.env.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
